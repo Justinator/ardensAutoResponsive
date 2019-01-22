a1e13a976113b9a0cd567e1ad4d93843773e23dd
Justinator <32310311+Justinator@users.noreply.github.com>
2019-01-22 06:01:50 -0600

    added about page

---
+++ page - about.php
@@ -0,0 +1,409 @@
+<?php
+/**
+ * Template Name: About
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package ardensAutoResponsive
+ */
+get_header();
+
+?>
+<div id="barba-wrapper">
+
+	<div class="barba-container">
+
+		<div id="primary" class="content-area">
+
+			<main id="main" class="site-main">
+
+				<section id="hero">
+
+					<div class="hero aboutHero">
+
+						<div class="heroOverlay">
+
+							<div class="heroHeadingWrap fullWidth">
+
+								<div class="centerText homeHeading">
+
+									<div class="heroHeading">
+
+										<h1 class="whiteText">A Family Owned and Operated Auto Repair Shop Since 1959</h1>
+
+									</div>
+
+								</div>
+
+							</div>
+
+						</div>
+
+					</div>
+
+				</section>
+
+				<section id="pageContent">
+
+				<div>
+
+				<section id ="ardensIntro">
+
+					<div class="navWidth flex-container centerAlignedContainer">
+
+						<div class="col50">
+
+							<div class="blockText">
+
+								<h2>About Arden's Automotive Hospital</h2>
+
+								<p>
+									Arden’s Automotive Hospital brings you three generations of auto repair experience. Arden’s originally started in 1959 by Arden ____ Todd, who specialized in
+									transmission repairs. In 1992, Arden ___ Todd took over his father's business keeping true to the transmission repair roots that his father had established.
+								</p>
+								<p>
+									As time went on, Arden’s began branching out from transmission work to encompass all auto repairs. Arden's became a full-service auto repair shop, servicing the Oshkosh
+									area, with more auto repair services to help you keep your car on the road longer.
+								</p>
+								<p>
+									2016 brought along the third generation in the family as Alex Todd reopened the business and took over where his Grandfather and father had left off. As Alex took over,
+									the founding principles of fairness and honesty remained the driving force behind Arden’s Automotive Hospital. So stop in or contact Arden’s
+									with all of your auto repair needs and experience the family oriented and honest service that has been driving Arden’s for almost 60 years.
+								</p>
+
+								<a href="/projects/"><button class="redButton">View Some Of Our Work</button></a>
+
+							</div>
+
+						</div>
+
+						<div class="col50">
+
+							<img src="/assets/scss/wp-content/uploads/2019/01/ardenAndAlexTodd.jpg" class="image">
+
+						</div>
+
+					</div>
+
+				</section>
+
+				<section id="ardensBenefits" class="paddedSection">
+
+					<div class="flex-container cardWrap">
+
+						<div class="col30">
+
+							<div class="raisedTextBox">
+
+								<div class="textWrap">
+
+									<h3 class="centerText">Trustworthy and Reliable</h3>
+
+									<p>No matter how big or small the issue, you’ll find a professional and honest opinion for all your auto repairs.</p>
+
+								</div>
+
+							</div>
+
+						</div>
+						<div class="col30">
+
+							<div class="raisedTextBox">
+
+								<div class="textWrap">
+
+									<h3 class="centerText">Family Oriented</h3>
+
+									<p>You’ll always be treated as a part of our family. Now in our third generation of ownership, family values run true here at Arden’s.</p>
+
+								</div>
+
+							</div>
+
+						</div>
+						<div class="col30">
+
+							<div class="raisedTextBox">
+
+								<div class="textWrap">
+
+									<h3 class="centerText">Fair</h3>
+
+									<p>Your car is important to you so it’s important to us as well, at Arden’s you’ll always get a fair price to get your car back on the road.</p>
+
+								</div>
+
+							</div>
+
+						</div>
+
+					</div>
+
+				</section>
+
+				<section id="services">
+
+					<div class="pageWidth">
+
+						<h2 class="centerText">Auto Repair Services in Oshkosh, WI</h2>
+
+					</div>
+
+					<div class="navWidth servicescontent">
+
+						<div class="serviceFlexContainer">
+
+							<div class="col30 zoomContainerWrapper">
+
+								<a class="zoomContainer" href="/auto-repair-services/oil-changes/">
+
+									<div class="zoomContainer">
+
+										<img class="image zoomImage" src="/assets/scss/wp-content/uploads/2019/01/oilChangeServicesMedium.jpg">
+
+									</div>
+
+									<h4 class="centerText">Oil Changes</h4>
+
+								</a>
+
+							</div>
+
+							<div class="col30 zoomContainerWrapper">
+
+								<a class="zoomContainer" href="/auto-repair-services/brake-repair/">
+
+									<div class="zoomContainer">
+
+										<img class="image zoomImage" src="/assets/scss/wp-content/uploads/2019/01/brakeRepairServicesMedium.jpg">
+
+									</div>
+
+									<h4 class="centerText">Brake Repair</h4>
+
+								</a>
+
+							</div>
+
+							<div class="col30 zoomContainerWrapper hiddenMobile">
+
+								<a class="zoomContainer" href="/auto-repair-services/air-conditioning/">
+
+									<div class="zoomContainer">
+
+										<img class="image zoomImage" src="/assets/scss/wp-content/uploads/2019/01/ACRepairServicesMoreSquare.jpg">
+
+									</div>
+
+									<h4 class="centerText">Air Conditioning</h4>
+
+								</a>
+
+							</div>
+
+						</div>
+
+					<div class="serviceFlexContainer">
+
+						<div class="col30 zoomContainerWrapper">
+
+							<a class="zoomContainer" href="/auto-repair-services/tires/">
+
+								<div class="zoomContainer">
+
+									<img class="image zoomImage" src="/assets/scss/wp-content/uploads/2019/01/tireRepairServicesMedium.jpg">
+
+								</div>
+
+								<h4 class="centerText">Tires</h4>
+
+							</a>
+
+						</div>
+
+						<div class="col30 zoomContainerWrapper">
+
+							<a class="zoomContainer" href="/auto-repair-services/exhaust/">
+
+								<div class="zoomContainer">
+
+									<img class="image zoomImage" src="/assets/scss/wp-content/uploads/2019/01/mufflerAndExhaustRepairServicesMedium.jpg">
+
+								</div>
+
+								<h4 class="centerText">Exhaust</h4>
+
+							</a>
+
+						</div>
+
+						<div class="col30 zoomContainerWrapper hiddenMobile">
+
+							<a class="zoomContainer" href="/auto-repair-services/preventative-maintenance/">
+
+								<div class="zoomContainer">
+
+									<img class="image zoomImage" src="/assets/scss/wp-content/uploads/2019/01/generalAutoMaintenanceMedium.jpg">
+
+								</div>
+
+								<h4 class="centerText">Preventative Maintenance</h4>
+
+							</a>
+
+						</div>
+
+						</div>
+
+						<div class="centerButton">
+
+							<a href="/auto-repair-services/"><button id="servicesButton" class="redButton">View all auto repair services</button></a>
+
+						</div>
+
+						<div class="fullWidth smallPaddedSection">
+
+							<div class="col60 centeredContainer">
+
+								<div class="raisedTextBox">
+
+									<div class="textWrap">
+
+										<h3>Client Feedback</h3>
+
+										<p>
+
+											" This a great family owned business that prides itself on honesty and trustworthiness. They really make you feel a part of the family when you're there. Prices
+											are always fair and you can trust that the work being done is of high quality. "
+
+											<br><br>
+
+											-Kailey Lemon
+										</p>
+
+									</div>
+
+								</div>
+
+							</div>
+
+						</div>
+
+					</div>
+
+				</section>
+
+				<section id="homepageCTA">
+
+					<div class="siteCTAWrap">
+
+						<div class="CTAWrapOverlay">
+
+								<div class="CTAcontent">
+
+									<h3 class="whiteText">Ready to start giving your car the expert care that it deserves?</h3>
+
+										<p class="whiteText">
+
+											Don't put it off any longer. Stop in or contact Arden's Automotive Hospital to set up an appointment for your vehicle today.
+
+										</p>
+
+
+										<div class="centeredButton">
+
+											<a href="/contact/"><button class="whiteButton">Contact Arden's</button></a>
+
+										</div>
+
+								</div>
+
+						</div>
+
+					</div>
+
+				</section>
+
+				<section class="map">
+
+					<div class="fullWidth">
+
+						<div>
+
+							<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2869.495470991913!2d-88.55277408449062!3d44.011154779110626!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8803ebf8a244848f%3A0xab246929a6800b92!2sArden&#39;s+Automotive+Hospital+LLC!5e0!3m2!1sen!2sus!4v1547119658458" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+
+						</div>
+
+							<div class="locationInfo">
+
+								<div class="locationContent">
+
+									<h5>Arden's Automotive Hospital</h5>
+
+									<a class="plainLink" target="_blank" href="https://www.google.com/maps/place/Arden's+Automotive+Hospital+LLC/@44.0111548,-88.5527741,17z/data=!3m1!4b1!4m5!3m4!1s0x8803ebf8a244848f:0xab246929a6800b92!8m2!3d44.0111548!4d-88.5505854">
+
+									<p>510 W 8th Ave<br>
+
+									Oshkosh, WI 54902</p></a>
+
+									<a class="plainLink" href="tel:(920)203-3694">(920) 203-3694</a>
+
+									<br>
+
+								</div>
+
+							</div>
+
+					</div>
+
+				</section>
+
+				<section class="feedback">
+
+					<div class="feedbackButtonWrapper">
+
+						<div class="feedbackContent">
+
+							<button class="redButton feedbackButton"><i class="fas fa-edit"></i> Feedback</button>
+
+						</div>
+
+					</div>
+
+					<div class="surveyWrapper">
+
+						<div class="surveyContent">
+
+							<button class="redButton surveyClose ">Close <i class="fas fa-times"></i></button>
+
+							<div class="survey">
+
+								<?php echo do_shortcode('[caldera_form id="CF5c387bcd0a786"]')?>
+
+							</div>
+
+						</div>
+
+					</div>
+
+				</section>
+
+			</section>
+
+		</div>
+
+	</main>
+
+	</div>
+
+</div>
+</div>
+
+<?php
+
+get_footer();
